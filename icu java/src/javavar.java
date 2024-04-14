486c4e4cc963348e679b9d89ef37871f19b48497
loganNickle <159865040+loganNickle@users.noreply.github.com>
2024-04-14 19:51:02 -0400

    Java

---
+++ icu java/src/javavar.java
@@ -0,0 +1,21 @@
+public class javavar {
+    /// class level variables
+        ///datatypes
+    
+        public static void main(String[]args) throws Exception{
+            ///datatype variables
+            int games = 3;
+            double allowance =40.50;    
+            long battlepass; ///dfine battle pass 
+                          
+    
+            battlepass = 15;///assined a valu to battle pass
+    
+            /// print out our varibules values along with a string that explains our values 
+    System.out.println("number of games that i own = " + games);
+    System.out.println("my mouthly allowance is =" + allowance);
+    System.out.println("the cost of a battlepass is =  " + battlepass);
+    
+    
+        }
+    }
